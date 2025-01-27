739c8b4095498955036dc61067a7c420e76058fe
Tan Kian-ting <chenjt30@gmail.com>
2025-01-27 16:42:57 +0800

    add detect char size from a font

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # uahgi
 
 another experimential typesetting tool
+
+## Known issues
+- Some .ttc file(eg. Noto Sans/Serif CJK) can't be loaded. It seems to be a libharu bug. For those want to use Noto Sans/Serif CJK, please download the .ttf file on Google Fonts. [Example (Trad. Chinese Version)](https://fonts.google.com/noto/specimen/Noto+Sans+TC)
---
+++ src/pdfoperating.jl
@@ -0,0 +1,136 @@
+module PDFOperating
+using libharu_jll
+using FreeType
+
+"""
+using TeX-like metrics, in px
+ - height
+ - weight
+ - depth
+"""
+struct FontMetrics
+    height::Float64
+    weight::Float64
+    depth::Float64    
+end
+
+#the default path is Noto Serif CJK Regular
+default_font_path = "src/thirdParty/fonts/noto/NotoSerifTC-Regular.ttf"
+#default_font_path = "src/thirdParty/fonts/linuxLibertine/LinLibertine_Rah.ttf"
+
+function createPDF()
+    #=returns pdf=#
+    pdf = @ccall libhpdf.HPDF_New(""::Cstring, 0::Cint)::Ptr{Cvoid}
+    #using utf-8
+    @ccall libhpdf.HPDF_UseUTFEncodings(pdf::Ptr{Cvoid})::Ptr{Cvoid}; 
+    @ccall libhpdf.HPDF_SetCurrentEncoder(pdf::Ptr{Cvoid}, "UTF-8"::Cstring)::Ptr{float}; 
+    return pdf
+end
+
+function addPage(pdf)
+    page = @ccall libhpdf.HPDF_AddPage(pdf::Ptr{Cvoid})::Ptr{Cvoid}
+    return page
+end
+
+function load_font(pdf, font_path,index=0)
+    HPDF_TRUE = 1
+    # only available in ttf and ttc
+    is_ttf = match(r".+\.ttf$", font_path)
+    is_ttc = match(r".+\.ttc$", font_path)
+    if is_ttf !== nothing
+        font_name = @ccall libhpdf.HPDF_LoadTTFontFromFile(
+            pdf::Ptr{Cvoid},
+            font_path::Cstring, HPDF_TRUE::Cint)::Cstring
+    elseif is_ttc !== nothing
+            font_name = @ccall libhpdf.HPDF_LoadTTFontFromFile2(
+            pdf::Ptr{Cvoid},
+            font_path::Cstring,
+            index::Cuint,
+            HPDF_TRUE::Cint)::Cstring
+            
+    else
+        throw("the format of \"$font_path\" is not valid!")
+    end
+    font = @ccall libhpdf.HPDF_GetFont(
+        pdf::Ptr{Cvoid},
+        font_name::Cstring,
+        "UTF-8"::Cstring)::Ptr{Cvoid};
+    
+    return font
+end
+
+function put_text(page, text, font, size, x, y)
+    # set font and size
+    @ccall libhpdf.HPDF_Page_SetFontAndSize(
+        page::Ptr{Cvoid},
+        font::Ptr{Cvoid},
+        float(size)::Cfloat)::Ptr{Cvoid};
+    
+    #declare putting text
+    @ccall libhpdf.HPDF_Page_BeginText(
+        page::Ptr{Cvoid})::Ptr{Cvoid}; #宣告加入文字
+    
+    # put the text on
+    @ccall libhpdf.HPDF_Page_TextOut(
+        page::Ptr{Cvoid},
+        float(x)::Cfloat,
+        float(y)::Cfloat,
+        text::Cstring)::Ptr{Cvoid};
+    
+    # end of putting text
+    @ccall libhpdf.HPDF_Page_EndText(
+        page::Ptr{Cvoid})::Ptr{Cvoid}; #宣告完成加入文字
+    
+end
+
+function save_pdf(pdf, file_name)
+    @ccall libhpdf.HPDF_SaveToFile(
+        pdf::Ptr{Cvoid},
+        file_name::Cstring)::Ptr{Cvoid};
+end
+
+function check_char_size(char, font_path, font_size, font_index=0)
+    library = Vector{FT_Library}(undef, 1)
+    error = FT_Init_FreeType(library)
+    face = Ref{FT_Face}()
+    FT_New_Face(library[1], font_path, font_index, face);
+    glyph_index = FT_Get_Char_Index(face[], char)
+    FT_Load_Glyph(face[], glyph_index, FT_LOAD_NO_SCALE)
+    faceRec = unsafe_load(face[])
+    glyphrec = unsafe_load(faceRec.glyph)
+
+    metrics = glyphrec.metrics
+    # horizonal mode
+    width = metricToPx(metrics.horiAdvance, font_size)
+    #from baseline up to top of the glyph
+    height = metricToPx(metrics.horiBearingY, font_size)
+    #from baseline down to the bottom of the glyph
+    depth = metricToPx(metrics.height - metrics.horiBearingY, font_size)
+
+    println("width ", width, "px")
+    println("height ", height, "px")
+    println("depth ", depth, "px")
+end
+
+"""
+convert freetype metric unit to px with fontsize in pt
+"""
+function metricToPx(metric, size_pt)
+    return metric * size_pt / 750
+end
+
+pdf = createPDF()
+page = addPage(pdf)
+font = load_font(pdf, default_font_path)
+put_text(page, "上下abc", font, 15, 100, 200)
+put_text(page, "天地人", font, 15, 200, 200)
+
+save_pdf(pdf, "text.pdf")
+
+println("PDF generated.")
+check_char_size(
+    'j',
+    default_font_path,
+    20)
+
+end
